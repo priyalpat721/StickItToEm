6535af99fab5cbf3c8a3d4065cfa36bc03c425b0
priyalpat721 <priyalpat721@gmail.com>
2022-03-14 21:58:10 -0400

    Firestore set up with code from documentation

--- app/src/main/java/edu/neu/madcourse/stickittoem/MainActivity.kt
+++ app/src/main/java/edu/neu/madcourse/stickittoem/MainActivity.kt
@@ -2,10 +2,62 @@ package edu.neu.madcourse.stickittoem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    private val db = Firebase.firestore
+    private val TAG = "FireStore"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Create a new user with a first and last name
+        val user1 = hashMapOf(
+            "first" to "Ada",
+            "last" to "Lovelace",
+            "born" to 1815
+        )
+
+        // Add a new document with a generated ID
+        db.collection("users")
+            .add(user1)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }
+
+        // Create a new user with a first, middle, and last name
+        val user2 = hashMapOf(
+            "first" to "Alan",
+            "middle" to "Mathison",
+            "last" to "Turing",
+            "born" to 1912
+        )
+
+        // Add a new document with a generated ID
+        db.collection("users")
+            .add(user2)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }
+
+        db.collection("users")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    Log.d(TAG, "${document.id} => ${document.data}")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error getting documents.", exception)
+            }
     }
 }
