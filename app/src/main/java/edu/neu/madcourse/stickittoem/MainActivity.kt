b17645deae1a9afd730d44f136d004af58065978
priyalpat721 <priyalpat721@gmail.com>
2022-03-17 04:29:38 -0400

    added a listener to main screen for now to get new activity

--- app/src/main/java/edu/neu/madcourse/stickittoem/MainActivity.kt
+++ app/src/main/java/edu/neu/madcourse/stickittoem/MainActivity.kt
@@ -1,64 +1,22 @@
 package edu.neu.madcourse.stickittoem
 
-import android.annotation.SuppressLint
-import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.ImageButton
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import edu.neu.madcourse.stickittoem.messages.StickerCard
-import edu.neu.madcourse.stickittoem.messages.StickerChatAdapter
-import java.text.SimpleDateFormat
-import java.util.*
-import kotlin.collections.ArrayList
+import edu.neu.madcourse.stickittoem.messages.MessagingActivity
 
 class MainActivity : AppCompatActivity() {
-
-    private var stickerMessageList: MutableList<StickerCard> = ArrayList()
-    private var recyclerView: RecyclerView? = null
-    private var adapter: StickerChatAdapter? = null
-    var context: Context = this
-    private lateinit var stickerDisplayButton: Button
-    private lateinit var sendButton: ImageButton
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_messaging)
-        setUpResources()
-        getDummyData()
+        setContentView(R.layout.activity_main)
 
-        stickerDisplayButton = findViewById(R.id.sticker_btn)
-        // TODO Jen: add the sticker popup functionality here :)
-        stickerDisplayButton.setOnClickListener {
-
-        }
-        sendButton = findViewById(R.id.send_btn)
-        sendButton.setOnClickListener{
-            // TODO logic is dependent on database
+        var button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            var intent = Intent(this, MessagingActivity::class.java)
+            startActivity(intent)
         }
     }
-
-    @SuppressLint("SimpleDateFormat")
-    private fun getDummyData() {
-        stickerMessageList.add(StickerCard("https://images.unsplash.com/photo-1647384630342-e2bffda01370?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80", SimpleDateFormat("dd/M/yyyy hh:mm:ss").format(
-            Date()
-        )))
-
-        stickerMessageList.add(StickerCard("https://images.unsplash.com/photo-1603826567438-7ccb36dff6e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1842&q=80", SimpleDateFormat("dd/M/yyyy hh:mm:ss").format(
-            Date()
-        )))
-    }
-
-    private fun setUpResources() {
-        recyclerView = findViewById(R.id.message_recycler_view)
-
-        adapter = StickerChatAdapter(stickerMessageList, context)
-        recyclerView!!.adapter = adapter
-        recyclerView!!.layoutManager = LinearLayoutManager(context)
-    }
-
 }
 
 //    private val db = Firebase.firestore
--- app/src/main/java/edu/neu/madcourse/stickittoem/messages/MessagingActivity.kt
+++ app/src/main/java/edu/neu/madcourse/stickittoem/messages/MessagingActivity.kt
@@ -1,5 +1,6 @@
 package edu.neu.madcourse.stickittoem.messages
 
+import android.annotation.SuppressLint
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,24 +9,44 @@ import android.widget.ImageButton
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import edu.neu.madcourse.stickittoem.R
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MessagingActivity : AppCompatActivity() {
     private var stickerMessageList: MutableList<StickerCard> = ArrayList()
     private var recyclerView: RecyclerView? = null
     private var adapter: StickerChatAdapter? = null
     var context: Context = this
-    private lateinit var stickerDisplayButton : Button
-    private lateinit var sendButton : ImageButton
+    private lateinit var stickerDisplayButton: Button
+    private lateinit var sendButton: ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_messaging)
+        setUpResources()
+        getDummyData()
 
         stickerDisplayButton = findViewById(R.id.sticker_btn)
+        // TODO Jen: add the sticker popup functionality here :)
+        stickerDisplayButton.setOnClickListener {
+
+        }
         sendButton = findViewById(R.id.send_btn)
+        sendButton.setOnClickListener{
+            // TODO logic is dependent on database
+        }
+    }
 
-        setUpResources();
+    @SuppressLint("SimpleDateFormat")
+    private fun getDummyData() {
+        stickerMessageList.add(StickerCard("https://images.unsplash.com/photo-1647384630342-e2bffda01370?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80", SimpleDateFormat("dd/M/yyyy hh:mm:ss").format(
+            Date()
+        )))
 
+        stickerMessageList.add(StickerCard("https://images.unsplash.com/photo-1603826567438-7ccb36dff6e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1842&q=80", SimpleDateFormat("dd/M/yyyy hh:mm:ss").format(
+            Date()
+        )))
     }
 
     private fun setUpResources() {
