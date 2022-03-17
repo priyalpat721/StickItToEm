851e2d66f6c796eb9b278d823e124078e601897b
priyalpat721 <priyalpat721@gmail.com>
2022-03-17 04:35:05 -0400

    added more dummy data to see if list was populating correctly

--- app/src/main/java/edu/neu/madcourse/stickittoem/messages/MessagingActivity.kt
+++ app/src/main/java/edu/neu/madcourse/stickittoem/messages/MessagingActivity.kt
@@ -47,6 +47,24 @@ class MessagingActivity : AppCompatActivity() {
         stickerMessageList.add(StickerCard("https://images.unsplash.com/photo-1603826567438-7ccb36dff6e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1842&q=80", SimpleDateFormat("dd/M/yyyy hh:mm:ss").format(
             Date()
         )))
+        stickerMessageList.add(StickerCard("https://images.unsplash.com/photo-1647396603763-1167b14aa7ea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=671&q=80", SimpleDateFormat("dd/M/yyyy hh:mm:ss").format(
+            Date()
+        )))
+
+        stickerMessageList.add(StickerCard("https://images.unsplash.com/photo-1647377106050-9dfad031a8e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80", SimpleDateFormat("dd/M/yyyy hh:mm:ss").format(
+            Date()
+        )))
+        stickerMessageList.add(StickerCard("https://images.unsplash.com/photo-1647391004047-92f60efd8a99?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80", SimpleDateFormat("dd/M/yyyy hh:mm:ss").format(
+            Date()
+        )))
+
+        stickerMessageList.add(StickerCard("https://images.unsplash.com/photo-1598362314166-a81c4a4ffa83?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=714&q=80", SimpleDateFormat("dd/M/yyyy hh:mm:ss").format(
+            Date()
+        )))
+
+        stickerMessageList.add(StickerCard("https://images.unsplash.com/photo-1610140755238-fdce8f3be7a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80", SimpleDateFormat("dd/M/yyyy hh:mm:ss").format(
+            Date()
+        )))
     }
 
     private fun setUpResources() {
