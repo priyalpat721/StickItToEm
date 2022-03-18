686a00a141a52c764b64296441851ef7c2346a04
priyalpat721 <priyalpat721@gmail.com>
2022-03-17 23:35:20 -0400

    added sign in/sign out functionality

--- app/src/main/java/edu/neu/madcourse/stickittoem/models/User.kt
+++ app/src/main/java/edu/neu/madcourse/stickittoem/models/User.kt
@@ -3,14 +3,27 @@ package edu.neu.madcourse.stickittoem.models
 class User {
     private lateinit var name: String
     private lateinit var email: String
+    private lateinit var password: String
+    private var totalStickersSent: Int = 0
+    private var totalStickersReceived: Int = 0
 
     constructor()
-    constructor(name: String, email: String) {
+
+    constructor(
+        name: String,
+        email: String,
+        password: String,
+        totalStickersSent: Int,
+        totalStickersReceived: Int
+    ) {
         this.name = name
         this.email = email
+        this.password = password
+        this.totalStickersSent = totalStickersSent
+        this.totalStickersReceived = totalStickersReceived
     }
 
     override fun toString(): String {
-        return "User(name='$name', email='$email')"
+        return "User(name='$name', email='$email', password='$password', totalStickersSent=$totalStickersSent, totalStickersReceived=$totalStickersReceived)"
     }
 }
