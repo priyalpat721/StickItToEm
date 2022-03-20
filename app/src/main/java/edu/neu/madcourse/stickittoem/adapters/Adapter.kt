754a96abccbb83b5d1c320613ecf24d44380dcff
rachitmehta96 <mehta.rac@northeastern.edu>
2022-03-20 09:37:37 -0400

    RM: deleted un-used java files

--- app/src/main/java/edu/neu/madcourse/stickittoem/adapters/Adapter.kt
+++ app/src/main/java/edu/neu/madcourse/stickittoem/adapters/Adapter.kt
@@ -5,10 +5,8 @@ import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import edu.neu.madcourse.stickittoem.fragments.chat.FragmentChat
-import edu.neu.madcourse.stickittoem.fragments.contacts.ContactsFragment
 import edu.neu.madcourse.stickittoem.fragments.contacts.FragmentContacts
 import edu.neu.madcourse.stickittoem.fragments.history.FragmentHistory
-import edu.neu.madcourse.stickittoem.fragments.history.HistoryFragment
 
 class Adapter(fragmentManager: FragmentManager, lifecycle: Lifecycle):FragmentStateAdapter(fragmentManager, lifecycle){
     override fun getItemCount(): Int {
