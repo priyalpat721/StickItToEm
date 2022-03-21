134b2cf339defc57a222a0b5cb3c4dc51397a8b9
priyalpat721 <priyalpat721@gmail.com>
2022-03-21 17:32:48 -0400

    stickers

--- app/src/main/java/edu/neu/madcourse/stickittoem/messages/BottomStickerSheetDialog.kt
+++ app/src/main/java/edu/neu/madcourse/stickittoem/messages/BottomStickerSheetDialog.kt
@@ -4,73 +4,40 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.Toast
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import edu.neu.madcourse.stickittoem.R
-import edu.neu.madcourse.stickittoem.adapters.StickerGridAdapter
 
 class BottomStickerSheetDialog : BottomSheetDialogFragment(){
-    private val imageList: MutableList<Int> = ArrayList<Int>()
-    private var recyclerView: RecyclerView? = null
-    var adapter: StickerGridAdapter? = null
-    //private lateinit var stickerGridAdapter: StickerGridAdapter
-    //private lateinit var gridLayoutManager: GridLayoutManager
+    private lateinit var button1 : ImageView
+    private lateinit var button2 : ImageView
+    private lateinit var button3 : ImageView
+    private lateinit var button4 : ImageView
+    private lateinit var button5 : ImageView
+    private lateinit var button6 : ImageView
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        super.onCreate(savedInstanceState)
+        // inflate the bottom sticker sheet layout
         return inflater.inflate(R.layout.bottom_sticker_sheet,container,false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        recyclerView = view.findViewById(R.id.sticker_recycler)
-
-        adapter = this.context?.let { StickerGridAdapter(view.context) }
-        recyclerView!!.adapter = adapter
-        recyclerView!!.layoutManager = GridLayoutManager(context,4,
-            LinearLayoutManager.VERTICAL,false)
-        imageList.clear()
-        imageList.add(com.google.firebase.messaging.R.drawable.adaptive_icon)
-        imageList.add(com.google.firebase.messaging.R.drawable.adaptive_icon)
-        imageList.add(com.google.firebase.messaging.R.drawable.adaptive_icon)
-        imageList.add(com.google.firebase.messaging.R.drawable.adaptive_icon)
-        imageList.add(com.google.firebase.messaging.R.drawable.adaptive_icon)
-        imageList.add(com.google.firebase.messaging.R.drawable.adaptive_icon)
-        imageList.add(com.google.firebase.messaging.R.drawable.adaptive_icon)
-        imageList.add(com.google.firebase.messaging.R.drawable.adaptive_icon)
-        imageList.add(com.google.firebase.messaging.R.drawable.adaptive_icon)
-        imageList.add(com.google.firebase.messaging.R.drawable.adaptive_icon)
-        imageList.add(com.google.firebase.messaging.R.drawable.adaptive_icon)
-        imageList.add(com.google.firebase.messaging.R.drawable.adaptive_icon)
-        imageList.add(com.google.firebase.messaging.R.drawable.adaptive_icon)
-        imageList.add(com.google.firebase.messaging.R.drawable.adaptive_icon)
-        imageList.add(com.google.firebase.messaging.R.drawable.adaptive_icon)
-        imageList.add(com.google.firebase.messaging.R.drawable.adaptive_icon)
-        imageList.add(com.google.firebase.messaging.R.drawable.adaptive_icon)
-        imageList.add(com.google.firebase.messaging.R.drawable.adaptive_icon)
-        imageList.add(com.google.firebase.messaging.R.drawable.adaptive_icon)
-        imageList.add(com.google.firebase.messaging.R.drawable.adaptive_icon)
-        
-        adapter?.setDataList(imageList)
-        /*adapter = view.findViewById(R.id.sticker_grid).also {
-            it.layoutManager = gridLayoutManager
-            it.setHasFixedSize(true)
-            it.adapter = alphaAdapters
-        }*/
-
-        //super.onViewCreated(view, savedInstanceState)
-        //recycler = view.findViewById(R.id.sticker_recycler)
-        /*button1 = view.findViewById(R.id.button1)
+        button1 = view.findViewById(R.id.button1)
         button2 = view.findViewById(R.id.button2)
-        button3 = view.findViewById(R.id.button3)*/
+        button3 = view.findViewById(R.id.button3)
+        button4 = view.findViewById(R.id.button4)
+        button5 = view.findViewById(R.id.button5)
+        button6 = view.findViewById(R.id.button6)
         // make click listeners for stickers
-        /*button1.setOnClickListener {
+        button1.setOnClickListener {
             Toast.makeText(context,"pressed button 1", Toast.LENGTH_LONG).show()
         }
         button2.setOnClickListener {
@@ -78,6 +45,15 @@ class BottomStickerSheetDialog : BottomSheetDialogFragment(){
         }
         button3.setOnClickListener {
             Toast.makeText(context,"pressed button 3", Toast.LENGTH_LONG).show()
-        }*/
+        }
+        button4.setOnClickListener {
+            Toast.makeText(context,"pressed button 4", Toast.LENGTH_LONG).show()
+        }
+        button5.setOnClickListener {
+            Toast.makeText(context,"pressed button 5", Toast.LENGTH_LONG).show()
+        }
+        button6.setOnClickListener {
+            Toast.makeText(context,"pressed button 6", Toast.LENGTH_LONG).show()
+        }
     }
 }
