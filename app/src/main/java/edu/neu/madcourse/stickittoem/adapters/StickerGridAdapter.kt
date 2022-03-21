8740a4aa4fcf7098da6823e59e886803679b363f
rachitmehta96 <mehta.rac@northeastern.edu>
2022-03-21 19:42:37 -0400

    worked with: Priyal Patel doesn't work: setup onClickListener for send button that connects to DB

--- app/src/main/java/edu/neu/madcourse/stickittoem/adapters/StickerGridAdapter.kt
+++ app/src/main/java/edu/neu/madcourse/stickittoem/adapters/StickerGridAdapter.kt
@@ -1,6 +1,8 @@
 package edu.neu.madcourse.stickittoem.adapters
 
 import android.content.Context
+import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,6 +10,7 @@ import android.widget.ImageView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import edu.neu.madcourse.stickittoem.R
+import edu.neu.madcourse.stickittoem.messages.StickerMessagingActivity
 import edu.neu.madcourse.stickittoem.messages.StickerModel
 
 class StickerGridAdapter(var context: Context) : RecyclerView.Adapter<StickerGridAdapter.ViewHolder>() {
@@ -43,7 +46,14 @@ class StickerGridAdapter(var context: Context) : RecyclerView.Adapter<StickerGri
 
         holder.image.setOnClickListener(){
             Toast.makeText(context, holder.description +" pressed", Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(context, StickerMessagingActivity::class.java)
+            intent.putExtra("image", data.image)
+            intent.putExtra("description", data.description)
+            context.startActivity(intent)
         }
+
+
     }
 
     override fun getItemCount() = dataList.size
