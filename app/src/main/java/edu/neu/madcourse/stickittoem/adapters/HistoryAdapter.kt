e3d0305f28a1007c397f86da19b36defa0275681
rachitmehta96 <mehta.rac@northeastern.edu>
2022-03-23 14:18:02 -0400

    history page layout done, skeleton for history recyclerview started

---
+++ app/src/main/java/edu/neu/madcourse/stickittoem/adapters/HistoryAdapter.kt
@@ -0,0 +1,25 @@
+package edu.neu.madcourse.stickittoem.adapters
+
+import android.content.Context
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import edu.neu.madcourse.stickittoem.cards.HistoryCard
+import edu.neu.madcourse.stickittoem.viewHolder.HistoryViewHolder
+
+class HistoryAdapter(
+    private var historyList: ArrayList<HistoryCard>,
+    private var context: Context
+) : RecyclerView.Adapter<HistoryViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HistoryViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: HistoryViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
--- app/src/main/java/edu/neu/madcourse/stickittoem/fragments/history/FragmentHistory.kt
+++ app/src/main/java/edu/neu/madcourse/stickittoem/fragments/history/FragmentHistory.kt
@@ -17,7 +17,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [FragmentHistory.newInstance] factory method to
  * create an instance of this fragment.
  */
-class FragmentHistory : Fragment() {
+class FragmentHistory : Fragment(R.layout.fragment_history2) {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
