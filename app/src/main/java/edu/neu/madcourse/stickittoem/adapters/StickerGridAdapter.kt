8edbf68a010ccc1c0b6a906bf25bcbc8857c402c
rachitmehta96 <mehta.rac@northeastern.edu>
2022-03-28 14:27:06 -0400

    cleaned up some code

--- app/src/main/java/edu/neu/madcourse/stickittoem/adapters/StickerGridAdapter.kt
+++ app/src/main/java/edu/neu/madcourse/stickittoem/adapters/StickerGridAdapter.kt
@@ -2,19 +2,13 @@ package edu.neu.madcourse.stickittoem.adapters
 
 import android.content.Context
 import android.content.Intent
-import android.content.res.Resources
-import android.graphics.drawable.Drawable
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
-import com.google.common.reflect.Reflection.getPackageName
 import edu.neu.madcourse.stickittoem.R
-import edu.neu.madcourse.stickittoem.cards.StickerCard
-import edu.neu.madcourse.stickittoem.messages.StickerMessagingActivity
 import edu.neu.madcourse.stickittoem.messages.StickerModel
 
 
@@ -52,9 +46,7 @@ class StickerGridAdapter(
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val data = dataList[position]
 
-        //holder.image.setImageResource(context.getResources().getIdentifier(data.image,"drawable",getPackageName()));
         holder.image.setImageResource(data.image)
-        //holder.image = data.image.toString()
         holder.description = data.description
 
         holder.image.setOnClickListener(){
