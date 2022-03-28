dc4b074ebfa052671ce428994e2da2f79793a7ae
jrwen <35855687+jrwen@users.noreply.github.com>
2022-03-27 23:18:54 -0400

    added constants for fcm

---
+++ app/src/main/java/edu/neu/madcourse/stickittoem/messages/notifications/Constants.kt
@@ -0,0 +1,9 @@
+package edu.neu.madcourse.stickittoem.messages.notifications
+
+class Constants {
+    companion object {
+        const val BASE_URL = "https://fcm.googleapis.com"
+        const val SERVER_KEY = "AAAAmT9eZxc:APA91bEUzh4cD0qqeNqzvMQv4EScFoTOcwBllfKVMjuPHWPkD5F8EVng6wE3UGxrpVAapsD336oGzp6dNUuK3rMYb1ZY7AQIjp0wo0cZEhAujwlnukmXTQQVQMoZ-vLaa6Zrq0GbY0xF"
+        const val CONTENT_TYPE = "application/json"
+    }
+}
